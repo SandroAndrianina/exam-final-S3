3e0bccf32418a66dcc5579e99f21e1310b223d46
JonathanRanaivoson <jonathanranaivoson8@gmail.com>
2026-02-16 17:01:47 +0300

    changement donnee de test

--- database/data.sql
+++ database/data.sql
@@ -15,20 +15,21 @@ INSERT INTO article (name, type, unit) VALUES
 
 
 INSERT INTO needs (city_id, article_id, quantity_requested, creation_date) VALUES 
-(1, 5, 1000000.00, '2026-02-16'), -- Tana demande 1.000.000 Ariary exemple ohatra
-(2, 3, 50.00, '2026-02-16'); -- Antsirabe demande 50 pièces de tôles 
+(1, 5, 1000000.00, '2026-02-16'), -- Tana (ID 1) demande 1M Ariary (ID 5)
+(2, 3, 50.00, '2026-02-16');      -- Antsirabe (ID 2) demande 50 tôles (ID 3)
 
 
--- Insertion des dons (gift)
--- Un donateur offre 1000kg de riz / Un autre offre 200 savons
+
 INSERT INTO gift (article_id, total_quantity, donation_date, description) VALUES 
-(1, 1000.00, '2023-10-05', 'Don anonyme - Stock riz'),
-(3, 200.00, '2023-10-06', 'Association Solidarite - Hygiène');
+(5, 2000000.00, '2026-02-15', 'Donation de la Banque'), -- Don ID 1 (Argent pour l'article 5)
+(3, 200.00, '2026-02-15', 'Stock Quincaillerie');       -- Don ID 2 (Tôles pour l'article 3)
 
 
--- Distribution pour Tana (800.000 Ariary pris sur le Don ID 1)
--- Distribution pour Antsirabe (30 tôles prises sur le Don ID 2)
+
+-- On donne 800.000 Ariary à Tana (Besoins ID 1) en piochant dans le Don d'argent (Gift ID 1)
+-- On donne 30. tôles à Antsirabe (Besoins ID 2) en piochant dans le Don de tôles (Gift ID 2)
 INSERT INTO distribution (gift_id, needs_id, attributed_quantity, affectation_date) VALUES 
 (1, 1, 800000.00, NOW()), 
 (2, 2, 30.00, NOW());
+
 
