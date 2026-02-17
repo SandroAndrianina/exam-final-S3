e8d308dc020da384f03823de2dc46ae6dad26132
Mihaja <andriamiharisoamihaja@gmail.com>
2026-02-17 13:30:59 +0300

    suppresion de list donnation

--- app/views/home.php
+++ app/views/home.php
@@ -17,7 +17,6 @@
                 <div class="sidebar">
                     <a href="/bngrc/list-cities" class="btn">List of cities</a>
                     <a href="/bngrc/list-gifts" class="btn">List of gifts</a>
-                    <a href="/bngrc/list-donations" class="btn">List of donations</a>
                     <a href="/bngrc/form-need" class="btn">Form of needs</a>
                     <a href="/bngrc/list-needs" class="btn">List of needs</a>
                     <a href="/bngrc/form-distribution" class="btn">Form of distribution</a>
